6221d081b4adbc7d887a1551e6cd0323b6bb469d
Ana Gavrilovic <ana.gavrilovic247@gmail.com>
2023-11-01 23:23:42 +0100

    Application dockerized

--- frontend/src/api/AxiosInstance.js
+++ frontend/src/api/AxiosInstance.js
@@ -1,3 +1,5 @@
 import axios from 'axios';
 
-export const axiosInstance = axios.create({ baseURL: 'http://localhost:8080/' });
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/';
+
+export const axiosInstance = axios.create({ baseURL: API_BASE_URL });
