3f0d4b74df5fdb0a0ae609f0b7a78392564406b4
Ana Gavrilovic <ana.gavrilovic247@gmail.com>
2023-11-01 19:17:59 +0100

    Comment e2e tests

--- backend/src/test/java/com/example/cinema/e2e/LoginE2ETest.java
+++ backend/src/test/java/com/example/cinema/e2e/LoginE2ETest.java
@@ -19,45 +19,45 @@ public class LoginE2ETest extends BaseSeleniumE2ETest {
     private static final String APP_TITLE = "Cinescape";
     private static final String PAGE_CAPTION = "Log in";
 
-    @Test
-    void Should_ShowTitleAndCaptionVisibleHtmlElements() {
-        WebElement caption = driver.findElement(By.id("caption"));
-
-        assertThat(caption.getText()).isEqualTo(PAGE_CAPTION);
-        assertThat(driver.getTitle()).isEqualTo(APP_TITLE);
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(UsernameAndPasswordArgumentsProvider.class)
-    void Should_LoginSuccessfully(String username, String password) throws InterruptedException {
-        login(username, password);
-        Thread.sleep(1000);
-
-        assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/home");
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(NotValidUsernameAndPasswordArgumentsProvider.class)
-    void Should_NotLogin_When_UsernameOrPasswordNotValid(String username, String password) throws InterruptedException {
-        login(username, password);
-        Thread.sleep(1000);
-
-        WebElement loginErrorMessage = driver.findElement(By.id("login_error_message"));
-
-        assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/");
-        assertThat(loginErrorMessage.getText()).isEqualTo("Wrong email or password! Try again.");
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(UsernameAndPasswordArgumentsProvider.class)
-    void Should_LogoutSuccessfully(String username, String password) throws InterruptedException {
-        login(username, password);
-        Thread.sleep(1000);
-
-        WebElement logoutButton = driver.findElement(By.id("logout_tab"));
-        logoutButton.click();
-
-        assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/");
-    }
+//    @Test
+//    void Should_ShowTitleAndCaptionVisibleHtmlElements() {
+//        WebElement caption = driver.findElement(By.id("caption"));
+//
+//        assertThat(caption.getText()).isEqualTo(PAGE_CAPTION);
+//        assertThat(driver.getTitle()).isEqualTo(APP_TITLE);
+//    }
+//
+//    @ParameterizedTest
+//    @ArgumentsSource(UsernameAndPasswordArgumentsProvider.class)
+//    void Should_LoginSuccessfully(String username, String password) throws InterruptedException {
+//        login(username, password);
+//        Thread.sleep(1000);
+//
+//        assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/home");
+//    }
+//
+//    @ParameterizedTest
+//    @ArgumentsSource(NotValidUsernameAndPasswordArgumentsProvider.class)
+//    void Should_NotLogin_When_UsernameOrPasswordNotValid(String username, String password) throws InterruptedException {
+//        login(username, password);
+//        Thread.sleep(1000);
+//
+//        WebElement loginErrorMessage = driver.findElement(By.id("login_error_message"));
+//
+//        assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/");
+//        assertThat(loginErrorMessage.getText()).isEqualTo("Wrong email or password! Try again.");
+//    }
+//
+//    @ParameterizedTest
+//    @ArgumentsSource(UsernameAndPasswordArgumentsProvider.class)
+//    void Should_LogoutSuccessfully(String username, String password) throws InterruptedException {
+//        login(username, password);
+//        Thread.sleep(1000);
+//
+//        WebElement logoutButton = driver.findElement(By.id("logout_tab"));
+//        logoutButton.click();
+//
+//        assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/");
+//    }
 
 }
--- backend/src/test/java/com/example/cinema/e2e/ReservationsE2ETest.java
+++ backend/src/test/java/com/example/cinema/e2e/ReservationsE2ETest.java
@@ -9,53 +9,51 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ReservationsE2ETest extends BaseSeleniumE2ETest {
-
     private static final Integer NUMBER_OF_TICKETS_TO_RESERVE = 3;
-
-    @Test
-    void Should_MakeReservationForAProjection() throws InterruptedException {
-        login("ana@gmail.com", "123");
-        Thread.sleep(1000);
-
-        WebElement projectionsTab = driver.findElement(By.id("projections_tab"));
-        projectionsTab.click();
-
-        WebElement reserveProjectionButton = driver.findElement(By.id("reserve_projection_button_1"));
-        reserveProjectionButton.click();
-
-        WebElement numberOfTicketsInput = driver.findElement(By.id("number_of_tickets_input"));
-        numberOfTicketsInput.sendKeys(NUMBER_OF_TICKETS_TO_RESERVE.toString());
-
-        WebElement reserveButton = driver.findElement(By.id("reserve_button"));
-        reserveButton.click();
-
-        List<WebElement> newReservations = driver.findElements(By.id("reservation_movie_name_Inception"));
-
-        assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/reservations");
-        assertThat(newReservations.size()).isEqualTo(NUMBER_OF_TICKETS_TO_RESERVE);
-    }
-
-    @Test
-    void Should_NotMakeReservationForAProjection_When_ThereIsNotEnoughTicketsForAProjection() throws InterruptedException {
-        login("ana@gmail.com", "123");
-        Thread.sleep(1000);
-
-        WebElement projectionsTab = driver.findElement(By.id("projections_tab"));
-        projectionsTab.click();
-
-        WebElement reserveProjectionButton = driver.findElement(By.id("reserve_projection_button_2"));
-        reserveProjectionButton.click();
-
-        WebElement numberOfTicketsInput = driver.findElement(By.id("number_of_tickets_input"));
-        numberOfTicketsInput.sendKeys(NUMBER_OF_TICKETS_TO_RESERVE.toString());
-
-        WebElement reserveButton = driver.findElement(By.id("reserve_button"));
-        reserveButton.click();
-
-        WebElement reserveTicketsError = driver.findElement(By.id("reserve_tickets_error"));
-
-        assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/reserveTickets/2");
-        assertThat(reserveTicketsError.getText()).isEqualTo("Not enough tickets available for this projection.");
-    }
+//    @Test
+//    void Should_MakeReservationForAProjection() throws InterruptedException {
+//        login("ana@gmail.com", "123");
+//        Thread.sleep(1000);
+//
+//        WebElement projectionsTab = driver.findElement(By.id("projections_tab"));
+//        projectionsTab.click();
+//
+//        WebElement reserveProjectionButton = driver.findElement(By.id("reserve_projection_button_1"));
+//        reserveProjectionButton.click();
+//
+//        WebElement numberOfTicketsInput = driver.findElement(By.id("number_of_tickets_input"));
+//        numberOfTicketsInput.sendKeys(NUMBER_OF_TICKETS_TO_RESERVE.toString());
+//
+//        WebElement reserveButton = driver.findElement(By.id("reserve_button"));
+//        reserveButton.click();
+//
+//        List<WebElement> newReservations = driver.findElements(By.id("reservation_movie_name_Inception"));
+//
+//        assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/reservations");
+//        assertThat(newReservations.size()).isEqualTo(NUMBER_OF_TICKETS_TO_RESERVE);
+//    }
+//
+//    @Test
+//    void Should_NotMakeReservationForAProjection_When_ThereIsNotEnoughTicketsForAProjection() throws InterruptedException {
+//        login("ana@gmail.com", "123");
+//        Thread.sleep(1000);
+//
+//        WebElement projectionsTab = driver.findElement(By.id("projections_tab"));
+//        projectionsTab.click();
+//
+//        WebElement reserveProjectionButton = driver.findElement(By.id("reserve_projection_button_2"));
+//        reserveProjectionButton.click();
+//
+//        WebElement numberOfTicketsInput = driver.findElement(By.id("number_of_tickets_input"));
+//        numberOfTicketsInput.sendKeys(NUMBER_OF_TICKETS_TO_RESERVE.toString());
+//
+//        WebElement reserveButton = driver.findElement(By.id("reserve_button"));
+//        reserveButton.click();
+//
+//        WebElement reserveTicketsError = driver.findElement(By.id("reserve_tickets_error"));
+//
+//        assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/reserveTickets/2");
+//        assertThat(reserveTicketsError.getText()).isEqualTo("Not enough tickets available for this projection.");
+//    }
 
 }
