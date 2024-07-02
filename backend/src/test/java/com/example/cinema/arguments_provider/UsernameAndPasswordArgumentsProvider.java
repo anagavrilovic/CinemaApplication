dbaa5f5aa9a5631caa5a25197eea0aabaa2dea9b
Ana Gavrilovic <ana.gavrilovic247@gmail.com>
2024-07-02 23:12:01 +0200

    added more test repetitions

--- backend/src/test/java/com/example/cinema/arguments_provider/UsernameAndPasswordArgumentsProvider.java
+++ backend/src/test/java/com/example/cinema/arguments_provider/UsernameAndPasswordArgumentsProvider.java
@@ -14,7 +14,31 @@ public class UsernameAndPasswordArgumentsProvider implements ArgumentsProvider {
                 Arguments.of("sanja@gmail.com", "123"),
                 Arguments.of("admin@gmail.com", "123"),
                 Arguments.of("srdjan@gmail.com", "123"),
-                Arguments.of("marija@gmail.com", "123")
+                Arguments.of("marija@gmail.com", "123"),
+                Arguments.of("user1@gmail.com", "123"),
+                Arguments.of("user2@gmail.com", "123"),
+                Arguments.of("user3@gmail.com", "123"),
+                Arguments.of("user4@gmail.com", "123"),
+                Arguments.of("user5@gmail.com", "123"),
+                Arguments.of("user6@gmail.com", "123"),
+                Arguments.of("user7@gmail.com", "123"),
+                Arguments.of("user8@gmail.com", "123"),
+                Arguments.of("user9@gmail.com", "123"),
+                Arguments.of("user10@gmail.com", "123"),
+                Arguments.of("user11@gmail.com", "123"),
+                Arguments.of("user12@gmail.com", "123"),
+                Arguments.of("user13@gmail.com", "123"),
+                Arguments.of("user14@gmail.com", "123"),
+                Arguments.of("user15@gmail.com", "123"),
+                Arguments.of("user16@gmail.com", "123"),
+                Arguments.of("user17@gmail.com", "123"),
+                Arguments.of("user18@gmail.com", "123"),
+                Arguments.of("user19@gmail.com", "123"),
+                Arguments.of("user20@gmail.com", "123"),
+                Arguments.of("user21@gmail.com", "123"),
+                Arguments.of("user22@gmail.com", "123"),
+                Arguments.of("user23@gmail.com", "123"),
+                Arguments.of("user24@gmail.com", "123")
         );
     }
 }
--- backend/src/test/java/com/example/cinema/constants/SharedConstants.java
+++ backend/src/test/java/com/example/cinema/constants/SharedConstants.java
@@ -2,8 +2,8 @@ package com.example.cinema.constants;
 
 public class SharedConstants {
 
-    public static final int numberOfUnitTestIterations = 1000;
-    public static final int numberOfIntegrationTestIterations = 30;
-    public static final int numberOfE2ETestIterations = 10;
+    public static final int numberOfUnitTestIterations = 500;
+    public static final int numberOfIntegrationTestIterations = 1000;
+    public static final int numberOfE2ETestIterations = 100;
 
 }
