1e3d232bfe279094f986511304aa718e2c302941
Ana Gavrilovic <ana.gavrilovic247@gmail.com>
2024-07-23 20:34:24 +0200

    remove clean gh-p from ci

--- backend/src/test/java/com/example/cinema/constants/SharedConstants.java
+++ backend/src/test/java/com/example/cinema/constants/SharedConstants.java
@@ -3,7 +3,7 @@ package com.example.cinema.constants;
 public class SharedConstants {
 
     public static final int numberOfUnitTestIterations = 500;
-    public static final int numberOfIntegrationTestIterations = 1000;
+    public static final int numberOfIntegrationTestIterations = 500;
     public static final int numberOfE2ETestIterations = 20;
 
 }
